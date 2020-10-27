dea32f91694266f605f4e994f6d8f7f40b2fc531
BlueWind <zero18030@gmail.com>
2020-10-27 23:36:36 +0900

    #4 広告実装 - build追加 - バナー広告

--- app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
+++ app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.bluewind.tarotsearchapp
 import android.os.Bundle
 import android.view.OrientationEventListener
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.main_fragment.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //AD初期化
+        MobileAds.initialize(this) {}
+        adMainView.loadAd(AdRequest.Builder().build())
+
         MyApplication.appActivity = this
-
-
         mOrientationEventListener = object : OrientationEventListener(this) {
             override fun onOrientationChanged(orientation: Int) {
                 MyApplication.deviceRotate = (orientation + 45) / 90 * 90
