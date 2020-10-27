2c61d348c1ae14e077d11f186b0430e38203def6
BlueWind <zero18030@gmail.com>
2020-10-28 00:04:54 +0900

    #4 Interstitial広告の追加

--- app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
+++ app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.bluewind.tarotsearchapp
 import android.os.Bundle
 import android.view.OrientationEventListener
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.main_fragment.*
 
@@ -17,6 +19,17 @@ class MainActivity : AppCompatActivity() {
         //AD初期化
         MobileAds.initialize(this) {}
         adMainView.loadAd(AdRequest.Builder().build())
+
+        //InterstitialAd
+        val mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712" //TODO : リリース時に変更
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        mInterstitialAd.adListener = object : AdListener() {
+            override fun onAdClosed() {
+                mInterstitialAd.loadAd(AdRequest.Builder().build())
+            }
+        }
+        MyApplication.appInterstitialAd = mInterstitialAd
 
         MyApplication.appActivity = this
         mOrientationEventListener = object : OrientationEventListener(this) {
