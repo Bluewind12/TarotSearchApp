96c06367b3ff1279e672fa640d010b3ea9508dd8
BlueWind <zero18030@gmail.com>
2020-10-31 13:59:37 +0900

    #9 初期起動の動作追加

--- app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
+++ app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.bluewind.tarotsearchapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.OrientationEventListener
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         MyApplication.appActivity = this
 
+        //起動時調査
+        startCheck()
 
         mOrientationEventListener = object : OrientationEventListener(this) {
             override fun onOrientationChanged(orientation: Int) {
@@ -28,5 +32,20 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         mOrientationEventListener.disable()
+    }
+
+    private fun startCheck() {
+        val sharedPreferences = getSharedPreferences("TarotDictionary", Context.MODE_PRIVATE)
+
+        if (sharedPreferences.getBoolean("FirstTimeStartup", true)) {
+            //ここで使い方の表示
+            AlertDialog.Builder(this)
+                .setMessage("テスト用")
+                .setTitle("テスト用")
+                .create()
+                .show()
+
+
+        }
     }
 }
