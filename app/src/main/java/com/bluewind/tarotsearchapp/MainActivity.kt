65573fb02719ddfcd099dca4f2d9e65f63e130c8
BlueWind <zero18030@gmail.com>
2020-11-04 01:44:46 +0900

    #9 説明文章をスライドで変更できるように - 残りはレイアウト作成のみ

--- app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
+++ app/src/main/java/com/bluewind/tarotsearchapp/MainActivity.kt
@@ -3,8 +3,13 @@ package com.bluewind.tarotsearchapp
 import android.content.Context
 import android.os.Bundle
 import android.view.OrientationEventListener
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.bluewind.tarotsearchapp.readme.ReadMeViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.read_me_tab_layout.*
+import kotlinx.android.synthetic.main.read_me_tab_layout.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -39,13 +44,25 @@ class MainActivity : AppCompatActivity() {
 
         if (sharedPreferences.getBoolean("FirstTimeStartup", true)) {
             //ここで使い方の表示
+//            AlertDialog.Builder(this)
+//                .setTitle("使い方")
+//                .setMessage("このアプリはスマートフォン・タブレットの向きを利用したアプリです。\n普通に持った状態でタロットをタップすると「正位置」の説明が\n逆向きにした状態でタロットをタップすると「逆位置」の説明が表示されます。")
+//                .create()
+//                .show()
+//
+
+            val view: View = layoutInflater.inflate(R.layout.read_me_tab_layout, null)
+            view.readMeViewPager.adapter = ReadMeViewPagerAdapter(supportFragmentManager, lifecycle)
+            TabLayoutMediator(
+                view.readMeTabLayout,
+                view.readMeViewPager,
+                TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                }
+            ).attach()
+
             AlertDialog.Builder(this)
-                .setTitle("使い方")
-                .setMessage("このアプリはスマートフォン・タブレットの向きを利用したアプリです。\n普通に持った状態でタロットをタップすると「正位置」の説明が\n逆向きにした状態でタロットをタップすると「逆位置」の説明が表示されます。")
-                .create()
+                .setView(view)
                 .show()
-
-
         }
     }
 }
